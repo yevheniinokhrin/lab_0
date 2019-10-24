f1a91b4b2ac1838482a491df678902d663f53fe2
yevheniinokhrin <56952711+yevheniinokhrin@users.noreply.github.com>
2019-10-24 16:45:52 +0300

    Add files via upload

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        List<String> list = new ArrayList<>(value.length());
+       for(int i=0; i< value.length();i++){
+           list.add(value);
+       }
+       list.add("");
+        return list;
     }
 
 }
